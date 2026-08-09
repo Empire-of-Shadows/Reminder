2baa805a9ec03403a4257165ff758230bd0e7130
EOSOClub <me@eosofficial.club>
2026-08-08 20:41:45 -0500

    cleanup

--- migrations/scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-"""ImperialReminder migration scripts.
-
-Each script is standalone and re-runnable:
-
-    python -m migrations.scripts.<name>            # dry run (default, no writes)
-    python -m migrations.scripts.<name> --apply    # perform the writes
-"""
